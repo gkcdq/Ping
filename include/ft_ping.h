06538fe4302901850a692e594ea7330d621bb921
Thomas Milin <tmilin@f2r6s20.clusters.42paris.fr>
2026-04-08 16:44:34 +0200

    42

--- include/ft_ping.h
+++ include/ft_ping.h
@@ -3,6 +3,7 @@
 
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/time.h>
 #include <stdio.h>
 #include <getopt.h>
 #include <stdlib.h>
--- src/main.c
+++ src/main.c
@@ -32,9 +32,9 @@ int main(int ac, char **av)
     struct addrinfo *res;
     memset(&hints, 0, sizeof(hints));
 
-    printf("AF_INET vaut : %d\n", AF_INET);         // Affiche 2
-    printf("SOCK_RAW vaut : %d\n", SOCK_RAW);       // Affiche 3
-    printf("IPPROTO_ICMP vaut : %d\n", IPPROTO_ICMP); // Affiche 1
+    // printf("AF_INET vaut : %d\n", AF_INET);         // Affiche 2
+    // printf("SOCK_RAW vaut : %d\n", SOCK_RAW);       // Affiche 3
+    // printf("IPPROTO_ICMP vaut : %d\n", IPPROTO_ICMP); // Affiche 1
 
     if (getaddrinfo(arc.host, NULL, &hints, &res) != 0)
         return (fprintf(stderr, "Error: DNS"), 1);
@@ -46,7 +46,11 @@ int main(int ac, char **av)
         perror("socket");
         return 1;
     }
-
+    struct timeval tv;
+    struct timezone tz;
+    unsigned int start_time = gettimeofday(&tv, &tz);
+    if (start_time)
+        return (printf("%d\n", start_time));
 }
 
 
